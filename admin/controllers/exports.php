0492edfc427e265810a0335700381048f4a3d4cc
Kornelius-Mg <corneillemg@gmail.com>
2020-06-07 13:47:43 +0200

    finish

---
+++ admin/controllers/exports.php
@@ -0,0 +1,36 @@
+<?php 
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+function exportToExcel(array $cours, $filiere_name)
+{
+    $spreadsheet = new Spreadsheet();
+    $feuille = 0;
+    foreach($cours as $promotion => $courses)
+    {
+        $myWorkSheet = new \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet($spreadsheet, $promotion);
+
+        $myWorkSheet->getCell("A1")->setValue("Intitule");
+        $myWorkSheet->getCell("A2")->setValue("Volume horaire");
+        $myWorkSheet->getCell("A1")->setValue("Categorie");
+
+        $lignes = 2;
+        $nb_cours = count($courses);
+        for($i = 0; $i < $nb_cours; $i++)
+        {
+            print_r($courses[$i]);
+            $myWorkSheet->setCellValueByColumnAndRow($lignes, 1, $courses[$i]["intitule"]);
+            $myWorkSheet->setCellValueByColumnAndRow($lignes, 2, $courses[$i]["volhoraire"]);
+            $myWorkSheet->setCellValueByColumnAndRow($lignes, 1, $courses[$i]["categorie_id"]);
+            $lignes++;
+        }
+
+        // Attach the "My Data" worksheet as the first worksheet in the Spreadsheet object
+        $spreadsheet->addSheet($myWorkSheet, $feuille);
+        $feuille++;
+    }
+    $writer = new Xlsx($spreadsheet);
+    $writer->save('/home/kornelius/'.$filiere_name.'.xlsx');
+}
